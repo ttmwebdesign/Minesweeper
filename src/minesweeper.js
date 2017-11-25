2faa1d390d0955f75f74e9273da84547adce5cc2
Niki Wolf <nikiwolf@gmail.com>
2017-11-25 15:21:26 -0700

    more work on the game

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,40 @@
-const exampleBoard = [
-[' ', ' ', ' '],
-[' ', ' ', ' '],
-[' ', ' ', ' ']
-];
-
-const printBoard = board => {
-  console.log('Current Board: ');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+	let board = [];
+	for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+		let row = [];
+		for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+			row.push(' ');
+		}
+		board.push(row);
+	}
+	return board;
 };
 
-printBoard(exampleBoard);
-exampleBoard[0][1] = '1';
-exampleBoard[1][0] = 'B';
-printBoard(exampleBoard);
+console.log('Player board:');
+console.log(generatePlayerBoard(3, 4));
+
+
+const gernerateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	let board = [];
+	for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+		let row = [];
+		for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+			row.push(null);
+		}
+		board.push(row);
+	}
+	return board;
+	const numberOfBombsPlaced = 0;
+	while (numberOfBombsPlaced < numberOfBombs) {
+		const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+		board[randomRowIndex][randomColumnIndex] = ' B ';
+
+		numberOfBombsPlaced++;
+	}
+
+
+};
+console.log('Bomb board:');
+console.log(gernerateBombBoard(3, 3, 2));
